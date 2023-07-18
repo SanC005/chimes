81b98f00685bedd64318dc11f1655319c01535de
SanC005 <sandeepcsrao@gmail.com>
2023-07-18 11:50:55 +0530

    added sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
-import Navbar from "@/components/navbar"
+// import Navbar from "@/components/navbar"
+
+import Sidebar from "@/components/Sidebar";
 import "../styles/global.css";
 export default function Home() {
   return (
     <main>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Sidebar/>
       <div className='text-center bold'>
         chimes
       </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,43 @@
+"use client"
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+
+import React from "react";
+
+
+function sidebar() {
+  return (
+    <Sidebar rootStyles={{
+     'height' :'100vh',
+     'background':'white',
+     'position':'absolute',
+      'top': '0',
+      'left': '0',
+      'padding':'0'
+    }}>
+    <Menu rootStyles={{
+      'height':'40vw',
+      'width':'100%',
+
+      'padding':'0',
+      // 'border':'black solid',
+      'background':'white',
+      'display':'flex',
+      // 'overflow':'auto'
+    }}>
+      
+      <SubMenu label="Charts">
+        <MenuItem> Pie charts </MenuItem>
+        <MenuItem> Line charts </MenuItem>
+      </SubMenu>
+      <MenuItem rootStyles={{'width':'100vw'}} className="bg-indigo-500"> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+      <MenuItem> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+      <MenuItem> Documentation </MenuItem>
+      <MenuItem> Calendar </MenuItem>
+    </Menu>
+  </Sidebar>
+  )
+}
+
+export default sidebar;
