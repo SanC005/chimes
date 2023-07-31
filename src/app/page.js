da3196659ab26141396942cae75a98489acff410
SanC005 <sandeepcsrao@gmail.com>
2023-08-01 01:40:23 +0530

    connect webpage to get all posts from db

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,30 @@ import Profile from "components/profile/profile";
 import Feedgrid from "components/public_feed/feedgrid";
 import NewSidebar from "components/sidebar/newSidebar";
 import { postDatas } from "components/public_feed/postdata";
-export default function Home() {
+// import Postingdata from "components/post/PostingData";
+async function getData() {
+  const res = await fetch('http://localhost:5001/api/v1/posts')
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+export default async function Home() {
+
+  const data = await getData()
+  const items = data.posts
+  // console.log(data)
+  // const box = items.map(item => <li key={item._id}>{item.title}</li>
+  // )
   return (
     <div className="">
       <Profile />
-      <div>Home Page</div>
-      {/* <Feedgrid data={postDatas}/> */}
+      <Feedgrid data={items}/>
     </div>
   );
 }
---
+++ src/components/post/postingData.js
@@ -0,0 +1,25 @@
+
+// async function getData() {
+//     const response = await fetch('http://reddit.com/r/popular.json?limit=10');
+  
+//     if (!response.ok) {
+//       const message = `An error has occured: ${response.status}`;
+//       throw new Error(message);
+//     }
+  
+//     const data = await response.json();
+//     return data;
+//   }
+//   export default async function Postingdata(){
+//         const items = await getData();
+//         console.log(items);
+//         // const boxes = items.map(item => {
+//         //     <div key={item.id}>{item.id}</div>
+//         // })
+//         return(
+//                 <div>
+//                     {/* {boxes} */}
+//                     click
+//                 </div>
+//         )
+//   }
