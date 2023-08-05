be1b49abb055abf55297de9a9ac96d7b75f689fa
SanC005 <sandeepcsrao@gmail.com>
2023-08-06 04:09:00 +0530

    added input to add new posts

--- src/components/post/addpost.jsx
+++ src/components/post/addpost.jsx
@@ -1,17 +1,37 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import { getLinkPreview } from "link-preview-js";
 import AddIcon from "@mui/icons-material/Add";
 function Addpost() {
+  const [linktext,setLinkText]= useState("");
+  const [linkdata,setLinkData]= useState("");
+  const changeLink = (e) => {
+      setLinkText(e)
+  }
   const addnew = () => {
-    alert("click");
+    alert(linktext)
+    getLinkPreview(linktext).then((data) =>{
+      console.log(data)
+      setLinkData(data)
+    }
+    );
+    console.log(setLinkData)
   };
   return (
     <div className="bg-green-400 text-center max-w-md m-auto w-full h-80 rounded-2xl hover:bg-green-500 active:bg-green-600">
       <div
-        className="flex h-full justify-center items-center cursor-pointer"
-        onClick={addnew}
+        className="flex h-full justify-center items-center cursor-pointer text-black"
+        
       >
-        <AddIcon sx={{ fontSize: 180 }} />
+        {/* <AddIcon sx={{ fontSize: 180 }} /> */}
+        <div>
+          <form className="flex flex-col gap-3">
+            <label className="">Link 
+            <input className="mx-2"type="text" onChange={(e) => changeLink(e.target.value)} />
+            </label>
+            <button onClick={addnew} className="bg-slate-400 m-1 p-1 rounded-md">Add</button>
+          </form>
+        </div>
       </div>
        
     </div>
