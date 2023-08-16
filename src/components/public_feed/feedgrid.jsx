c46c27499bcb51d9e496734acafc1f9b88ecc748
SanC005 <sandeepcsrao@gmail.com>
2023-08-17 02:48:29 +0530

    fixed vercel hosting issue

--- src/components/public_feed/feedgrid.jsx
+++ src/components/public_feed/feedgrid.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Post from "../post/post";
 import Addpost from "../post/addpost";
-import { useTheme, useUpdateTheme } from "utils/themeContext";
+
 
 
 function Feedgrid({ data }) {
-  const darkTheme = useTheme();
-  const toggleTheme = useUpdateTheme();
   const post = data?.map((postData) => (
     <li className="p-2" key={postData.id}>
       <Post
@@ -19,7 +17,7 @@ function Feedgrid({ data }) {
     </li>
   ));
   return (
-    <div className={` ${darkTheme?"bg-purple-400":"bg-cyan-500"} grid grid-cols-1 list-none p-2 md:grid-cols-2 lg:grid-cols-3`}>
+    <div className={`bg-cyan-500 grid grid-cols-1 list-none p-2 md:grid-cols-2 lg:grid-cols-3`}>
       <div className="p-2">
         <Addpost data={data}/>
       </div>
